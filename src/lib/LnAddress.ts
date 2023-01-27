e5a04e2343953ca50265f714673a0d56c4ebead0
Michael Little <michael@wrtdesign.net>
2023-01-27 12:40:42 +1100

    cleaned up LnAddress

--- src/lib/LnAddress.ts
+++ src/lib/LnAddress.ts
@@ -4,15 +4,9 @@ import { Utils } from "./Utils";
 
 class LnAddress {
   static addressToUrl(address: string): string | false {
-    //const LNAddressRE = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     const LNAddressRE = /([^@]+)@(.+)/;
     const m = address.match(LNAddressRE);
-    logger.debug("matches", m);
-    //if (m && m.length >= 6) {
     if (m && m.length >= 2) {
-      logger.info("LnAddress: success ", address);
-
-      //return `https://${m[5]}/.well-known/lnurlp/${m[1]}`;
       return `http://${m[2]}/.well-known/lnurlp/${m[1]}`;
     }
 
@@ -26,17 +20,15 @@ class LnAddress {
     const url = LnAddress.addressToUrl(address);
 
     if (url) {
-      logger.debug("calling", url);
       const resp = await Utils.get(url);
-      logger.debug("resp", resp.data.callback);
+
       if (resp.status >= 200 && resp.status < 400) {
-        logger.debug("lnurl called", resp.data);
         if (resp.data.callback && resp.data.tag == "payRequest") {
           const cbUrl = new URL(resp.data.callback);
           cbUrl.searchParams.set("amount", String(amount));
-          logger.debug("calling url ", cbUrl);
+
           const cbResp = await Utils.get(cbUrl.toString());
-          logger.debug("cbResp", cbResp);
+
           if (cbResp.status >= 200 && cbResp.status < 400) {
             if (cbResp.data && cbResp.data.pr) {
               return cbResp.data.pr;
